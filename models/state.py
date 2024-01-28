4b887465807525093cc7f4a1e7a4e66958d83a93
derfal <sohail.bounnite@gmail.com>
2024-01-28 23:36:24 +0100

    fix errors

--- models/state.py
+++ models/state.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
+"""
+Defines the State class.
+"""
+
+import models
+from os import getenv
+from models.base_model import Base, BaseModel
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
 
+class State(BaseModel, Base):
+    """
+    Represents a state for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    
-    # For DBStorage: Relationship with City
-    cities = relationship('City', back_populates='state', cascade='all, delete-orphan')
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            cities = models.storage.all(City).values()
+            filcities = [city for city in cities if city.state_id == self.id]
+            return filcities
+
+    @property
+    def cities(self):
+        """getters from storage linked to the current State"""
+        city_obj = models.storage.all(City)
+        return [city for city in city_obj.values() if city.state_id == self.id]
